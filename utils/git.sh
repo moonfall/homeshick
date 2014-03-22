ce2b9f6298092db00dbe1917086c2a9599c0387b
David Riley <driley@moonfall.com>
2014-03-21 21:00:45 -0700

    allow push with an explicit repo to push regardless of config

--- utils/git.sh
+++ utils/git.sh
@@ -100,7 +100,7 @@ function pull {
 	return $EX_SUCCESS
 }
 
-function push {
+function push_all {
 	[[ ! $1 ]] && help_err push
 	local castle=$1
 	local repo="$repos/$castle"
@@ -112,6 +112,21 @@ function push {
 		ignore 'unavailable'
 		return $EX_SUCCESS
 	fi
+
+	local git_out
+	git_out=$(cd "$repo"; git push 2>&1)
+	[[ $? == 0 ]] || err $EX_SOFTWARE "Unable to push $repo. Git says:" "$git_out"
+
+	success
+	return $EX_SUCCESS
+}
+
+function push {
+	[[ ! $1 ]] && help_err push
+	local castle=$1
+	local repo="$repos/$castle"
+	pending 'push' $castle
+	castle_exists 'push' $castle
 
 	local git_out
 	git_out=$(cd "$repo"; git push 2>&1)
